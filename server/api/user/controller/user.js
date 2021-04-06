0d91d7c4e1393312cb15c6132b1a50d36c5930be
Henry Faure-Geors <h.f-g@hotmail.fr>
2021-04-06 19:38:58 +0200

    login bon

--- server/api/user/controller/user.js
+++ server/api/user/controller/user.js
@@ -27,7 +27,7 @@ exports.login = async (req,res, next) => {
     const pw = req.body.password
     try{ 
         const token = await user_query.checkUser(username,pw)
-        res.status(201).json({token})
+        res.status(200).json({token})
     }
     catch(err){
         res.status(400).json({message: err.message})
